609805537a0a93213bdf3fc3b197cfa9008812d2
aswathypsmca <aswathyps@mca.ajce.in>
2017-10-13 16:50:48 +0530

    version 3
    payment page

--- addcustomer_edit.php
+++ addcustomer_edit.php
@@ -8,8 +8,8 @@ header('location:index.php');
 }
 if(isset($_POST['submit']))
 {
-$a=$_POST["em"];//email
-$b=$_POST["ch"];//chitty name
+$a=$_POST["em"];
+$b=$_POST["ch"];
 //echo($a);
 //echo($b);
 //SELECT `cust_id`, `c_name`, `c_gender`, `c_address`, `c_mob`, `c_email`, `pan`, `log_id`, `status` FROM `tbl_customer` WHERE
@@ -20,30 +20,26 @@ $cid=$row1['cust_id'];
 //echo($cid);
 
 //SELECT `c_id`, `chitt_name`, `installment`, `divisions`, `chitt_amount`, `s_date`, `status` FROM `tbl_chittreg` WHERE
-$sql2="SELECT `c_id` FROM `tbl_chittreg` WHERE chitt_name='$b'";
+$sql2="SELECT * FROM `tbl_custchitty` WHERE c_id='$chid' and cust_id= '$c_id' ";
+//SELECT `cust_ch_id`, `c_id`, `cust_id`, `status` FROM `tbl_custchitty` WHERE 1
+
 $re2=mysqli_query($con,$sql2);
 $row2=mysqli_fetch_array($re2);
 $chid=$row2['c_id'];
-//echo($chid);
-$sql21="SELECT `cust_id` FROM `tbl_custchitty` WHERE `cust_id`='$cid'";
-$re5=mysqli_query($con,$sql21);	
-$row3=mysqli_fetch_array($re5);
-//print_r($row3);
-$c1=$row3['cust_id'];
-if($c1==" ")
+$check1=mysqli_num_rows($re2);
+if($check1<1)
 {
-	
+//echo($chid);
 //INSERT INTO `tbl_custchitty`(`cust_ch_id`, `c_id`, `cust_id`, `status`) VALUES ([value-1],[value-2],[value-3],[value-4])
 $sql4="INSERT INTO `tbl_custchitty`(`c_id`, `cust_id`, `status`) VALUES ('$chid', '$cid', '0')";
 $re4=mysqli_query($con,$sql4);
-echo $sql4;
-	
 }
 else
 {
-	echo("already exists!!!!");
-
+	$msg="exist";
+echo "<script>alert('$msg');</script>";
 }
+//echo $sql4;
 }
 ?>
 <!DOCTYPE html>
@@ -66,7 +62,7 @@ else
   </div>
   <!-- Div for body section -->
  <div class="body1">
-   
+   <!-- <center><img src="ajce.png" id="ajce_img"></center>-->
 <div class="body_text1">
 <a href="stafflogin.php">HOME<img src="home.jpg" id="homeimg" width="28px" height="15px"></a><br><br><br>
  <a href="cust_view.php">view</a><br><br>
@@ -87,11 +83,12 @@ else
   
    
 <form action="#" method="post">
+
 <tr><td> SELECT USER NAME:</td><td><select name="sl">
 	<option>select</option>
 	
   <?php
-$sq2="SELECT * FROM `tbl_customer`";
+$sq2="SELECT * FROM `tbl_customer`where status='1'";
 $result2=mysqli_query($con,$sq2);
 while($row=mysqli_fetch_array($result2))
 {
@@ -115,7 +112,8 @@ $sql5="SELECT * FROM  `tbl_customer` WHERE `c_email`='$a'";
 
 $result5=mysqli_query($con,$sql5);
 $r5=mysqli_fetch_array($result5);
-echo $r5['c_name'];
+//echo $r5['c_name'];
+}
 ?>
 
                
@@ -126,13 +124,21 @@ echo $r5['c_name'];
         <tr><td> Email:</td> <td><input type="text"  value="<?php echo $r5['c_email'] ?>" name="em" id="em"></td></tr>
 		  
  
+	<!--<tr><td> NAME:</td> <td><input type="text" name="cname"placeholder="Name" required></td></tr></br><tr></tr>
+	<tr><td> GENDER:</td><td>  <input type="radio" name="c_gender" value="male" checked> Male &nbsp&nbsp
+                               <input type="radio" name="c_gender" value="female"> Female</td></tr></br><tr></tr>
+	
+	<tr><td> ADDRESS:</td> <td><input type="text" name="caddr"placeholder="Address" required></td></tr></br><tr></tr>
+	<tr><td> PHONE:</td> <td><input type="phno" name="cmob" maxlength="10"placeholder="mob num" required></td></TR></br></tr><tr></tr>
+	<tr><td> EMAIL:</td> <td><input type="email" name="cemail" placeholder="mail_id" required></td></tr></br><tr></tr>
+	<tr><td> PAN:</td> <td><input type="text" name="cpan" placeholder="PAN" required></td></TR></br></tr><tr></tr>
 	
 	
+	
+	-->
 	<tr><td> SELECT CHITTY:</td><td><select name="ch">
 	<option>select</option>
-	<?Php
-	}
-?>
+	
   <?php
 $sql="SELECT * FROM `tbl_chittreg`";
 $result=mysqli_query($con,$sql);
@@ -147,6 +153,7 @@ while($row=mysqli_fetch_array($result))
 }
 ?>
 </select></td></tr></br><tr></tr><br><br><br>
+	
 	<tr><td><input type="submit" name="submit" value="ADD"></td></tr><tr></tr>	
   
 	</table>
---
+++ chittypayment.php
@@ -0,0 +1,155 @@
+<?php
+include 'dbcon.php';
+
+	if(!(isset($_SESSION['user_name'])))
+	{
+
+	//header('location:index.php');
+	}
+
+		if(isset($_POST['submit']))
+		{
+			$a=$_POST["chitname"];
+			$b=$_POST["install"];
+			$c=$_POST["division"];
+			$d=$_POST["amount"];
+			//echo $d;
+			$e=$_POST["sdate"];
+	$sql1="INSERT INTO `tbl_chittreg` (`chitt_name`, `installment`, `divisions`, `chitt_amount`, `s_date`, `status`)
+			VALUES ('$a','$b','$c','$d','$e','0')";
+	$result1=mysqli_query($con,$sql1);
+
+	$q11 = "SELECT `c_id` FROM `tbl_chittreg` WHERE `chitt_name`=$a ";
+
+	$row1 = mysqli_query($con,$q11);
+		}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>chitty management</title>
+  <link rel="stylesheet" href="logo.css">
+  </head>
+	<body>
+	<!-- Div for header section -->
+		<div class="header">
+		<!--<img class="logo" src="logo1.png" alt="Logo"/>-->
+		<br>
+		<b class="heading1"><strong><font color="yellow" size="6">GRAMIN CHITTY MANAGEMENT</font></strong></b>
+		<br>
+		<b class="heading2"><strong><font color="blue" size="4">GOVT.OF KERALA</font></strong></b>
+		</div>
+  <!-- Div for body section -->
+		<div class="body1" >
+		<!-- <center><img src="ajce.png" id="ajce_img"></center>-->
+			<div class="body_text1">
+			<a href="stafflogin.php">HOME<img src="home.jpg" id="homeimg" width="28px" height="15px"></a><br><br>
+			<br><br>
+			<a href="logout.php">LOGOUT</a><br>
+			</div>
+		</div>
+  <!-- Div for login section -->
+		<div class="login2">
+		<!--<center><h2 class="login_head">-->
+			<div class="body_text3">
+	<form method="post" name="myform" action="#">
+			<table style="width:200% cellpadding:1%  ">
+			<tr><td><h3>CHITTY PAYMENT</h3></td></tr>
+			<tr><td> CHITTY NAME:</td> 
+			<td><select name="chitty" onchange="getId(this.value);">
+				<option value="">select chitty</option>
+			<?php
+				$query = "SELECT * FROM tbl_chittreg ";
+				$results = mysqli_query($con, $query);
+			while($row=mysqli_fetch_array($results)) 
+			{
+			?>
+				<option value="<?php echo $row["c_id"];?>"> <?php echo $row["chitt_name"]; ?> </option>
+			<?php
+				echo $row["chitt_name"];
+			}
+			?>
+			
+				</select>
+				
+				 
+
+		</td></tr></br><tr></tr>
+		<tr><td>Customer ID:</td><td>
+		<select name="customer" id="customer" onchange="getId1(this.value);">
+        <option value=" ">select id</option>
+		</select></br><tr></tr>
+		
+	<tr><td> NAME:</td> <td><input type="text" id="cname"></td></tr></br><tr></tr> 
+	<tr><td> ADDRESS:</td> <td><input type="text" id="caddr"></td></tr></br><tr></tr>
+	<tr><td> PHONE:</td> <td><input type="text" id="cmob" maxlength="10" ></td></TR></br></tr><tr></tr>
+	
+	<tr><td> INSTALLMENT NO:</td> <td> <input type="varchar" name="instalno" placeholder="Total installment" required></td></tr></br><tr></tr>
+	<tr><td> AMOUNT:</td> <td><input type="varchar" name="amount" required></td></tr></br><tr></tr>
+	<tr><td> DIVIDENT:</td> <td><input type="varchar" name="divident" required></td></tr></br><tr></tr>
+	<tr><td> PAYING DATE:</td> <td><input type="date" name="sdate"required></td></tr></br><tr></tr>
+	<tr><td> STAFF NAME:</td><td><select>
+	<option>select</option>
+		<?php
+		$sql="SELECT * FROM `tbl_staff`";
+		$result=mysqli_query($con,$sql);
+		while($row=mysqli_fetch_array($result))
+		{
+	//$i=$row['log_id'];
+	//echo($row['chitt_name']);
+		?>
+			<option><?php echo $row['s_name'] ?></option>
+    
+		<?php
+		}
+		?>
+	<tr><td ><input type="submit" name="submit" value="PAY">&nbsp&nbsp<input type="reset" name="clear" value="RESET"></td></tr><tr></tr>	
+	&nbsp&nbsp</table>
+	</form>
+		</div></div></div>
+
+	<div class="footer">
+    <center><marquee>GRAMIN CHITTY MANAGEMENT</marquee></center>
+    </div>
+	<script src="js/jquery.js"></script>
+<script>
+function getId(val){
+
+  $.ajax({
+    type: "POST",
+    url: "getdata.php",
+    data: "cust_id="+val,
+    success: function(data){
+	//	alert(data);
+      $("#customer").html(data);
+
+}
+});
+}
+
+ function getId1(val){
+
+  $.ajax({
+    type: "POST",
+    url: "getdata1.php",
+    data: "Did="+val,
+    success: function(data){
+		var aa =data.split(":");
+		alert(aa[0]);
+		$('#cname').val(aa[0]);
+				$('#caddr').val(aa[1]);
+
+						$('#cmob').val(aa[2]);
+
+     // $("#village").html(data);
+
+}
+});
+} 
+</script>
+
+    </body>
+    </html>
